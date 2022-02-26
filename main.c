25f9d91aad01d352c18d7252f85e0f6169474471
joinatan <eklund.jonatan@gmail.com>
2022-02-26 13:12:10 +0100

    far ljud nu

--- main.c
+++ main.c
@@ -6,42 +6,66 @@
 #define PR2VALUE (80000000 / 256 / 1) //100 ms
 
 
+int freq = 40;
 int counter = 0;
 void isr_routine(void)
 {
     /* SPI */
     if(IFS(1) & (1<<6))
     {
+        IFSCLR(1) = (1<<6);
+        counter++;
+        if(counter > (freq/2)){
+
         PORTDCLR = (1<<1);
-        SPI2BUF = 0x0F;
+        SPI2BUF = 0xFF;
         PORTDSET = (1<<1);
+        }else{
+
+        PORTDCLR = (1<<1);
+        SPI2BUF = 0x00;
+        PORTDSET = (1<<1);
+        }
+        if(counter == freq){
+            counter = 0;
+        }
+
+
+        /* SPI2BUF = 0x00; */
+        PORTDCLR = (1<<1);
     }
     //clock
-    if(IFS(0) & (1<<8))
-    {
-        IFSCLR(0) = (1<<8);
-        counter++;
+    /* if(IFS(0) & (1<<8)) */
+    /* { */
+    /*     IFSCLR(0) = (1<<8); */
+    /*     counter++; */
 
-        if(counter == 10);
-        {
-        /* PORTDINV = (1<<1); */
-        counter = 0;
-        }
-    }
+    /*     if(counter == 10); */
+    /*     { */
+    /*     /1* PORTDINV = (1<<1); *1/ */
+    /*     counter = 0; */
+    /*     } */
+    /* } */
     return;
 }
 
 void setup(void)
 {
     /* --------------SPI SETTINGS------------------- */
-    SPI2CON = 0; //clear spi
+    /* SPI2CONCLR = 0xFFFFFFFF; //clear spi */
+    SPI2CONCLR = (1<<31);
     SPI2CONSET = (1<<28); //MSSEN not avaliable?
+    /* SPI2CONSET = (1<<16); // master mode */
+    SPI2CONSET = (1<<7); // master mode
     SPI2CONSET = (1<<5); // master mode
+    /* SPI2CONSET = (1<<23); */
+    SPI2BRG = 85; //baud rate 2.5MHz
+    SPI2CONSET = (0<<2);
     SPI2CONSET = (1<<15); //SPI ON
 
     /* ------spi flags ---------- */
-    IFSCLR(2) = (1<<6); //tx flag 
     IECSET(1) = (1<<6); //tx interupt
+    IFSCLR(1) = (1<<6); //tx flag 
     IPCSET(7) = (7<<26); //highest priority
     IPCSET(7) = (3<<24);
 
@@ -74,6 +98,7 @@ int main(){
     setup();
     /* PORTDSET = (1<<1); */
 
+    SPI2BUF = 0x0F;
     while(1)
     {};
 
